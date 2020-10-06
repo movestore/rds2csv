b0cc083509851f588218f08e0facb65b8e1b758e
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2020-10-06 12:44:11 +0200

    add csv save path

--- RFunction.R
+++ RFunction.R
@@ -6,7 +6,7 @@ rFunction <- function(data)
   
   data.csv <- as.data.frame(data)
   data.csv.nona <- data.csv[,!sapply(data.csv, function(x) all(is.na(x)))] 
-  write.csv(data.csv.nona,"data.csv",row.names=FALSE)
+  write.csv(data.csv.nona, file = paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"data.csv"),row.names=FALSE)
   
   result <- data
   return(result)
