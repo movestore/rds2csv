b35dd33db25565cda03c5e4ec69bf05d1fca440e
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2021-10-26 10:23:14 +0200

    fix names

--- RFunction.R
+++ RFunction.R
@@ -6,8 +6,11 @@ rFunction <- function(data)
   
   data.csv <- as.data.frame(data)
   data.csv.nona <- data.csv[,!sapply(data.csv, function(x) all(is.na(x)))]
-  infos.pr <-c("local_identifier","timestamp","location_long","location_lat")
-  infos.pr.ix <- which(names(data.csv.nona) %in% c("local_identifier","timestamp","location_long","location_lat"))
+  
+  names(data.csv.nona) <- make.names(names(data.csv.nona),allow_=FALSE)
+  
+  infos.pr <-c("trackId","timestamp","location.long","location.lat")
+  infos.pr.ix <- which(names(data.csv.nona) %in% c("trackId","timestamp","location.long","location.lat"))
   data.csv.nona.pr <- data.frame(data.csv.nona[,infos.pr],data.csv.nona[,-infos.pr.ix])
     
   write.csv(data.csv.nona.pr, file = paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"data.csv"),row.names=FALSE)
